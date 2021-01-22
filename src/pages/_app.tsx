d8c0141f10618e985707bd45f4c99ace606c88af
John Letey <johnletey@gmail.com>
2021-01-22 09:49:56 +0000

    feat: use system colour mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,20 @@
 import { GeistProvider, CssBaseline } from "@geist-ui/react";
+import { useState, useEffect } from "react";
 
 function App({ Component, pageProps }) {
+  const [theme, setTheme] = useState("");
+  useEffect(() => {
+    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
+      setTheme("light");
+    } else {
+      setTheme("dark");
+    }
+  }, []);
+
   return (
     <GeistProvider
       theme={{
-        type: "dark",
+        type: theme,
       }}
     >
       <CssBaseline />
