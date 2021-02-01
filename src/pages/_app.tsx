0967698594f5475ceb940b128562aafc36bd6854
John Letey <johnletey@gmail.com>
2021-02-01 08:46:00 +0000

    fix: don't flash light mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,12 @@
 import { GeistProvider, CssBaseline } from "@geist-ui/react";
-import { useState, useEffect } from "react";
 import Head from "next/head";
 
 function App({ Component, pageProps }) {
-  const [theme, setTheme] = useState("");
-  useEffect(() => {
-    if (window.matchMedia("(prefers-color-scheme: light)").matches) {
-      setTheme("light");
-    } else {
-      setTheme("dark");
-    }
-  }, []);
+  let theme = "light";
+  if (typeof window === `undefined`) return "light";
+  if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    theme = "dark";
+  }
 
   return (
     <GeistProvider
